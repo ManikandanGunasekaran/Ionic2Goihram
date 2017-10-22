e7e2d19e82a012baf46526439200d6e17878ad7a
ManikandanGunasekaran <manibecse11@gmail.com>
2017-10-22 10:43:38 +0530

    replaced app page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -9,7 +10,9 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-
+import { TrackmapPage } from '../pages/trackmap/trackmap';
+import { FriendsPage } from '../pages/friends/friends';
+import { SignupPage } from '../pages/signup/signup';
 
 
   // Initialize Firebase
@@ -28,10 +31,14 @@ import { LoginPage } from '../pages/login/login';
   declarations: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    TrackmapPage,
+    FriendsPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(config),
     AngularFireAuthModule
@@ -40,7 +47,10 @@ import { LoginPage } from '../pages/login/login';
   entryComponents: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    TrackmapPage,
+    FriendsPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
