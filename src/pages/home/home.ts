f5ab8722a8cd4582ddef7334a530a58ed6b9bcb6
ManikandanGunasekaran <manibecse11@gmail.com>
2017-10-22 10:39:46 +0530

    replacing files in home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,66 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-declare var google:any;
+import { TrackmapPage } from '../trackmap/trackmap';
+import { FriendsPage } from '../friends/friends';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit {
-  map: any;
-  markers = [];
-  startpoint = new google.maps.LatLng(13.038039, 80.21597);
-  endpoint = new google.maps.LatLng(13.138039, 80.31597);
+export class HomePage {
+  
   constructor(public navCtrl: NavController) {
   }
-
-  ngOnInit() {
-  this.initMap();
+  public MoveToTrackPage(){
+      this.navCtrl.push(TrackmapPage);
   }
-  //current location
-  private initMap() {
-  
-  let divMap = (<HTMLInputElement>document.getElementById('map'));
-  this.map = new google.maps.Map(divMap, {
-  center: this.startpoint,
-  zoom: 15,
-  disableDefaultUI: true,
-  draggable: false,
-  zoomControl: true
-  });
-  
-  this.createMapMarker(this.startpoint);
-  }
-  
-  //marker for current location
-  private createMapMarker(place:any):void {
-  var marker = new google.maps.Marker({
-  map: this.map,
-  position: place
-  });
-  this.markers.push(marker);
-  }
-
-  //getting directions
-  calculateAndDisplayRoute() {
-    let directionsService = new google.maps.DirectionsService;
-    let directionsDisplay = new google.maps.DirectionsRenderer;
-    
-    directionsDisplay.setMap(this.map);
-
-    directionsService.route({
-      origin: this.startpoint,
-      destination: this.endpoint,
-      travelMode: google.maps.TravelMode['DRIVING']
-  }, (res, status) => {
-
-      if(status == google.maps.DirectionsStatus.OK){
-          directionsDisplay.setDirections(res);
-      } else {
-          console.warn(status);
-      }
-
-  });
+  public MoveToFriendsPage(){
+      this.navCtrl.push(FriendsPage);
   }
 }
